a89a2d000ea6d63e57d251d2fc28d6c54182f63e
Snir Yehuda <snir@savvy.security>
2023-01-09 23:30:25 +0200

    try theme

---
+++ _posts/2023-01-09-my-first-post.markdown
@@ -0,0 +1,22 @@
+---
+layout: single
+title:  "Hello world"
+date:   2023-01-09 22:50:09 +0200
+categories: jekyll update fun
+---
+
+lets gets this party stated
+`snippet?`
+```go 
+var f string
+f = "what"
+```
+
+# title?
+## sub1 
+* do this
+* to that
+
+- [ ] checkbox?
+- [X] checkboxV
+
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2023/01/09/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "Hello world",
+        "excerpt":"lets gets this party stated snippet?  var f string f = \"what\"   title?  sub1     do this        to that       checkbox?   checkboxV   ","categories": ["jekyll","update","fun"],
+        "tags": [],
+        "url": "/jekyll/update/fun/2023/01/09/my-first-post.html",
+        "teaser": null
+      }]
